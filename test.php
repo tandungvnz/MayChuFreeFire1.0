465ffc7a193c738b3280e3d62116ea8c7fe4e37b
tandungvnz <tandungvnz@gmail.com>
2026-07-28 00:59:35 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 DB_HOST=localhost        # Server MySQL
-DB_USER=root            # Username MySQL
-DB_PASSWORD=            # Password (để trống nếu không có)
+DB_USER=dev_user            # Username MySQL
+DB_PASSWORD=dev_pass         # Password (để trống nếu không có)
 DB_NAME=test_db         # Tên database
 DB_PORT=3306            # Port MySQL
